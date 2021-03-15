9463ac68ffd225dad17bdfb20b6d851110c0331b
mattkyea <kyeamatt@gmail.com>
2021-03-15 17:50:57 -0400

    now using Twitter4J, Java wrapper around Twitter API

--- Android/app/src/main/java/com/example/assign5/MainActivity.java
+++ Android/app/src/main/java/com/example/assign5/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.assign5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
 import android.os.Bundle;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // The factory instance is re-useable and thread safe.
+
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    //Twitter twitter = TwitterFactory.getSingleton();
+                    List<Status> statuses = null;
+                    try {
+                        Paging paging = new Paging(1, 100);
+                        statuses = twitter.getUserTimeline("elonmusk", paging);
+                    } catch (TwitterException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Showing elon's timeline.");
+                    for (Status status : statuses) {
+                        System.out.println(status.getUser().getName() + ":" + status.getText() + " at " + status.getCreatedAt());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+
+
     }
 
     //updates, as I have no code here yet
@@ -20,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
     //https://api.twitter.com/2/users/:id/tweets?max_results=100&tweet.fields=attachments,created_at,text,entities
     //but I also need to send my token
     //and some of the tweet.fields might change
+
+    //update 2
+    //decided that using Twitter4J would be easier
+    //its basically a Java wrapper around Twitter's API
+    //i've done the same thing in the past (Spotipy for Python around Spotify's API) and liked it much more
 
 
 
