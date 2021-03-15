64a17a87be5bfaa319376b4e48c45cde9e6318ef
mattkyea <kyeamatt@gmail.com>
2021-03-15 17:21:44 -0400

    comments detailing my progress with Twitter API

--- Android/app/src/main/java/com/example/assign5/MainActivity.java
+++ Android/app/src/main/java/com/example/assign5/MainActivity.java
@@ -11,4 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //updates, as I have no code here yet
+    //- I set up a twitter developer account to access the API
+    //- And found what request I need using Postman to test
+    //- text will be easy, but I don't think I'll be able to do pictures easily
+    //API link will be something like
+    //https://api.twitter.com/2/users/:id/tweets?max_results=100&tweet.fields=attachments,created_at,text,entities
+    //but I also need to send my token
+    //and some of the tweet.fields might change
+
+
+
 }
